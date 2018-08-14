2ef42d32f908848c55c16dc5a6f81f643c888d31
Yutong <yutong.wang.94@gmail.com>
2018-08-14 17:30:35 -0400

    basic scatterplot
    autofix lint

---
+++ src/parts/label.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Label = ({ x, y, children }) => {
+  return (
+    <text
+      fill='black'
+      textAnchor='middle'
+      x={x}
+      y={y}
+      style={{
+        fontSize: '0.9rem'
+      }}
+    >
+      {children}
+    </text>
+  )
+}
+
+Label.propTypes = {
+  // required
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+  children: PropTypes.node.isRequired,
+}
+
+export default Label
